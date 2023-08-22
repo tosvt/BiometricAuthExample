122d61181973842ebd254b75865ec5222831108b
tosvt <sweb7@ya.ru>
2023-08-22 17:02:57 +0300

    Код реализации функционала

--- app/src/main/java/com/example/biometricauthexample/MainActivity.kt
+++ app/src/main/java/com/example/biometricauthexample/MainActivity.kt
@@ -1,11 +1,84 @@
 package com.example.biometricauthexample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import com.example.biometricauthexample.databinding.ActivityMainBinding
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promtInfo: BiometricPrompt.PromptInfo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imgFinger.setOnClickListener{
+            checkDeviceHasBiometric()
+        }
+
+        executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor, object : BiometricPrompt.AuthenticationCallback(){
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                Toast.makeText(this@MainActivity,"Ошибка авторизации: $errString", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                Toast.makeText(this@MainActivity,"Авторизация успешно пройдена!", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                Toast.makeText(this@MainActivity,"Авторизация не удалась", Toast.LENGTH_LONG).show()
+            }
+        })
+        promtInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Какой-то заголовок")
+            .setSubtitle("Какой-то подзаголовок")
+            .setNegativeButtonText("Отменить авторизацию")
+            .build()
+
+        binding.btnLogin.setOnClickListener {
+            biometricPrompt.authenticate(promtInfo)
+        }
+    }
+
+    fun checkDeviceHasBiometric(){
+        val biometricManager = BiometricManager.from(this)
+        when(biometricManager.canAuthenticate(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)){
+            BiometricManager.BIOMETRIC_SUCCESS -> {
+                Log.d("BIO_AUTH", "App can authenticate using biometrics")
+                binding.tvMsg.text = "Биометрическая авторизация доступна"
+                binding.btnLogin.isEnabled = true
+            }
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
+                Log.d("BIO_AUTH", "Biometric feautures are currently unavailable")
+                binding.tvMsg.text = "Биометрическая авторизация недоступна"
+                binding.btnLogin.isEnabled = false
+            }
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+                    putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                        BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
+                }
+                binding.btnLogin.isEnabled = false
+
+                startActivityForResult(enrollIntent, 100)
+            }
+        }
     }
 }
